fa7771bcf21a1a1c923b78789bf776b4e2520492
Alan Cristian Martinez Genaro <martinezgenarocristian@gmail.com>
2020-10-07 07:45:28 -0500

    agrege cambio

--- Adiosmundo.java
+++ Adiosmundo.java
@@ -15,6 +15,7 @@ public class Adiosmundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Cambio");
         // TODO code application logic here
     }
     
